0d964ff80d9526d7fa5b741b47af83083ff1c363
Jorge Duarte <sadujorge@gmail.com>
2024-02-28 18:04:00 -0800

    ESLint and configuration to project.

--- src/ts/database.ts
+++ src/ts/database.ts
@@ -1,11 +1,11 @@
 export const setQuestion = () => {
     const randomNumber = Math.floor(Math.random() * 5)
     const questions = [
-        { id: 1, text: "Is free will real or just an illusion?" },
-        { id: 2, text: "Would you rather know the universe's secrets or know every language?" },
-        { id: 3, text: "Does technology advance humanity or hinder it?" },
-        { id: 4, text: "Is a life that focuses on avoiding pain a good and worthwhile life?" },
-        { id: 5, text: "If you could live forever, would you want to?" },
+        { id: 1, text: 'Is free will real or just an illusion?' },
+        { id: 2, text: 'Would you rather know the secrets of the universe or know every language?' },
+        { id: 3, text: 'Does technology advance humanity or hinder it?' },
+        { id: 4, text: 'Is a life that focuses on avoiding pain a good and worthwhile life?' },
+        { id: 5, text: 'If you could live forever, would you want to?' },
     ]
     const questionCard = document.getElementById('question-card');
     if (questionCard !== null) {
