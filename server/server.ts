764399926b818d51ac0fb07db376f25756c9b488
Jorge Duarte <sadujorge@gmail.com>
2024-02-29 17:59:29 -0800

    added express server and configured basic typescript flow

---
+++ server/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('../client/dist'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
