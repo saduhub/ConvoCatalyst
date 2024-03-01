b754664d5ce8471c202604d655e1479bd21509de
Jorge Duarte <sadujorge@gmail.com>
2024-02-29 18:58:49 -0800

    Added home route.

--- server/server.js
+++ server/server.js
@@ -5,8 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.use(express_1.default.static('../client/dist'));
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
+app.get('/', (req, res) => res.sendFile('../client/dist/index.html'));
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
